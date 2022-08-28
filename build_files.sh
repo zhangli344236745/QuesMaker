e41a8223b4992fb5cf3837fdc6be8afda1a34004
Keshav Sharma <108651949+keshav-xebia@users.noreply.github.com>
2022-08-28 16:40:00 +0530

    Removed sudo

--- build_files.sh
+++ build_files.sh
@@ -1,6 +1,6 @@
 # build_files.sh
-sudo apt-get install libsqlite3-dev sqlite-devel
-./configure --enable-loadable-sqlite-extensions && make && sudo make install
+apt-get update && apt-get install libsqlite3-dev sqlite-devel
+./configure --enable-loadable-sqlite-extensions && make && make install
 pip install pysqlite
 pip install -r requirements.txt
 python3.9 manage.py collectstatic --noinput
