893ed43f5d39700b89f0cb5dc4772f0e1c9d95c3
Keshav Sharma <108651949+keshav-xebia@users.noreply.github.com>
2022-08-28 17:18:44 +0530

    Removed yum update

--- build_files.sh
+++ build_files.sh
@@ -1,8 +1,6 @@
 # build_files.sh
-yum update -y
 yum install sqlite-devel -y
 ./configure --enable-optimizations --enable-loadable-sqlite-extensions
 make install
-pip install pysqlite
 pip install -r requirements.txt
 python3.9 manage.py collectstatic --noinput
