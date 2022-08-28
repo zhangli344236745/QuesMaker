16bd21eccb44f05619d2e193059f93166317bb4f
Keshav Sharma <108651949+keshav-xebia@users.noreply.github.com>
2022-08-28 17:37:33 +0530

    Added cd commands

--- build_files.sh
+++ build_files.sh
@@ -1,6 +1,9 @@
 # build_files.sh
+ls
+cd Python-*/
 yum install sqlite-devel -y
 ./configure --enable-optimizations --enable-loadable-sqlite-extensions
-make install
+make && make altinstall
+cd ~
 pip install -r requirements.txt
 python3.9 manage.py collectstatic --noinput
