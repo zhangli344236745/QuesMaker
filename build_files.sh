0c261bff53da171bfa96c9879c5bc4390622f7fb
Keshav Sharma <108651949+keshav-xebia@users.noreply.github.com>
2022-08-28 16:43:10 +0530

    Added hostnamectl

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,5 @@
 # build_files.sh
+hostnamectl
 apt-get update && apt-get install libsqlite3-dev sqlite-devel
 ./configure --enable-loadable-sqlite-extensions && make && make install
 pip install pysqlite
